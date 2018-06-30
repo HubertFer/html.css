595f7319275b1077310d90fa271c014c059d162b
Hubert Mac <hubert@Huberts-MacBook-Pro-2.local>
2018-06-29 23:27:31 -0300

    Files Upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste Page for CSS</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>Title of my Page</h1>
+	This is the text typed on my page...<br/>
+	Another text to fill it up...
+	<img src="google.jgp" />
+
+</body>
+</html>
